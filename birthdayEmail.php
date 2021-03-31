efcea1c800a7739fe3b228067257beacdb09f7e6
tripathidhaval <77092801+tripathidhaval@users.noreply.github.com>
2021-03-31 12:14:36 +0530

    Task2 - Send birthday email before one day to user

---
+++ birthdayEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * A command line cron job that send email to users one day before their birthday
+ */
+
+// Initialize Joomla framework
+const _JEXEC = 1;
+
+// Load system defines
+if (file_exists(dirname(__DIR__) . '/defines.php'))
+{
+	require_once dirname(__DIR__) . '/defines.php';
+}
+
+if (!defined('_JDEFINES'))
+{
+	define('JPATH_BASE', dirname(__DIR__));
+	require_once JPATH_BASE . '/includes/defines.php';
+}
+
+// Get the framework.
+require_once JPATH_LIBRARIES . '/import.legacy.php';
+
+// Bootstrap the CMS libraries.
+require_once JPATH_LIBRARIES . '/cms.php';
+
+/**
+ * Cron job to trash expired cache data.
+ *
+ * @since  2.5
+ */
+class birthdayEmail extends JApplicationCli
+{
+	/**
+	 * Entry point for the script
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
+	public function doExecute()
+	{
+		$cache = JFactory::getCache();
+		$cache->gc();
+	}
+}
+
+JApplicationCli::getInstance('GarbageCron')->execute();
